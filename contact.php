09bd5b3e1dc5b05a79e5788c809476d6378ccc24
Julian Nicholls <juliannicholls29@gmail.com>
2015-07-18 21:43:06 +0100

    Added contact page. Moved img contents to images.

---
+++ contact.php
@@ -0,0 +1,71 @@
+<?php
+/*
+  Copyright © Julian Nicholls 2010-2015, All Rights Reserved.
+  Licensed for use on one site: reallybigshoe.co.uk
+*/
+
+//---------------------------------------------------------------------------
+// Defined Values
+//---------------------------------------------------------------------------
+
+    define( 'DESTINATION',    'julian@reallybigshoe.co.uk' );
+    define( 'FROM_FORM',      'info@reallybigshoe.co.uk' );
+    define( 'FROM_EMAIL',     'Really Big Shoe Contact Form <' . FROM_FORM . '>' );
+    define( 'FROM_HEADER',    'From: ' . FROM_EMAIL . "\n" );
+
+    ini_set( 'SMTP', 'mail.reallybigshoe.co.uk' );
+    ini_set( 'sendmail_from', FROM_FORM );
+
+//---------------------------------------------------------------------------
+
+    if( $_SERVER['HTTP_HOST'] == 'localhost:8080' )
+    {
+        echo '<pre>' . print_r($_POST, TRUE) . '</pre>';
+        return;
+    }
+
+    $subject = isset($_POST['subject']) ? $_POST['subject'] : '(No Subject)';
+
+    $body .= "Name:          " . $_POST['uname'];
+    $body .= "\nEmail Address: " . $_POST['email'];
+    $body .= "\n\nSubject:       $subject";
+    $body .= "\nMessage\n\n  " . $_POST['message'];
+
+// Send it
+
+    if($_POST['uname'] == '' || !mail(DESTINATION, "Really Big Shoe Contact - $subject" , $body, FROM_HEADER))
+    {
+        if(isset($_GET['a']))   // Ajax request
+            echo "FAILED";
+        else
+        {
+            html_header();
+            echo '<h1>Error 500</h1><p>There is currently a problem with sending messages.</p>';
+            echo '<p>Please try again later.</p>';
+            html_footer();
+        }
+    }
+    elseif(isset($_GET['a']))      // Ajax request
+        echo "OK";
+    else
+        header("Location: http://reallybigshoe.co.uk/contact.html");
+
+
+function html_header()
+{
+    echo '<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>Really Big Shoe: Mail Problem/title>
+</head>
+
+<body>
+';
+}
+
+
+function html_footer()
+{
+    echo "\n</body><\n</html>\n";
+}
---
+++ js/mail.js
@@ -0,0 +1,47 @@
+// Copyright (C) Julian Nicholls 2010-2015, All rights reserved.
+// Licensed for use on one site: reallybigshoe.co.uk
+
+$(function() {
+    $(".noblank").attr("title", "This field cannot be blank");
+
+    $("#contact-form").submit(function() {
+        var ok = true;
+
+        $(".noblank").each(function() {
+            if(trim($(this).val()) === '') {
+                $(this).css('border', '2px solid red');
+                ok = false;
+            }
+        });
+
+        if(ok) {
+            $("#contact-send").fadeOut(600);
+            $.post("contact.php?a=1", $("#contact-form").serialize(), function(data) {
+                var $info = $("#contact-holder > p");
+
+                // alert( 'data: "' + data + '"' );
+
+                $info.css({ 'fontSize': '200%', 'color': '#1a1' });
+
+                if(data.substr(0, 2) == "OK") {
+                    $('#contact-form').fadeOut(600);
+                    $info.text("Thank you. We will be in touch soon.");
+                }
+                else {
+                    $info.html("Something has gone wrong with sending messages.<br>Please try again later.")
+                         .css('color', '#ff2020');
+                    $("#contact-send").show();
+                }
+            });
+        }
+        else {
+            alert( 'You must fill in the indicated fields.')
+        }
+
+        return  false;  // Submit has been done
+    });
+});
+
+function trim( str ) {
+	return str.replace( /^\s+|\s+$/g, '' );
+}
